eefb3d500176f5d59da9f1aa2b3d78093cab7411
victor.chu <victorchu0610@outlook.com>
2019-12-01 16:56:01 +0800

    parser combinators

--- src/main/java/info/victorchu/Combinators.java
+++ src/main/java/info/victorchu/Combinators.java
@@ -1,8 +1,28 @@
 package info.victorchu;
 
+import info.victorchu.result.Success;
+
+import java.util.function.Function;
+
 /**
  * a collection of parser combinator functions
  */
 public abstract class Combinators {
+
+    /**
+     * return combinator
+     * succeeds immediately without consuming any input, and return supplied value.
+     * @param x
+     * @param <I>
+     * @param <R>
+     * @return
+     */
+    public static <I,R> Parser<I,R> retn(R x){
+        return ((Parser<I, R>) input -> new Success<>(input, x)).named("return -> "+x);
+    }
+
+    public static <I,R,U> Parser<I,U> bind(Parser<I,R> parser, Function<R,Parser<I,U>> function){
+        
+    }
 
 }
